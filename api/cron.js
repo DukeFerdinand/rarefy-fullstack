5694a239a4b7d18e1abb2c16755fc9110655c392
Doug Hooker <DouglassHooker@gmail.com>
2022-12-18 00:35:34 -0800

    Another attempt

--- api/cron.js
+++ api/cron.js
@@ -1,16 +1,15 @@
-// import playwright from 'playwright-aws-lambda';
-import puppeteer from 'puppeteer';
 import edgeChromium from 'chrome-aws-lambda';
 
 const TEST_URL =
 	'https://buyee.jp/item/search/query/%E5%B2%A9%E5%B4%8E%E5%AE%8F%E7%BE%8E%20Me%20too/category/22260?translationType=1';
 
 async function testBrower() {
-	const browser = await puppeteer.launch({
+	const browser = await edgeChromium.puppeteer.launch({
 		args: edgeChromium.args,
 		defaultViewport: edgeChromium.defaultViewport,
 		executablePath: await edgeChromium.executablePath,
-		headless: true
+		headless: true,
+		ignoreHTTPSErrors: true
 	});
 
 	const page = await browser.newPage();
